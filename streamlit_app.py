0b4a719f0148c7f51eb819c198be4a59527ab01e
jonc1987 <117519739+jonc1987@users.noreply.github.com>
2024-09-28 17:23:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Jonathan Is The Best Person In The WORLD!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "REFUNDS GARUNTTEED"
 )
